71e2d6ca2b9be17ad78ffe764863b01bd785266a
Marcos <marped273@alu.centroafuera.es>
2022-03-23 10:28:16 +0100

    Ejercicio Terminado X23/03

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,26 @@
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 
 public class Main {
 public static void main(String[] args){
 
-    String fakeInput = "1111";
+    String fakeInput = "1111\n2222\n1234";
     String pass = "1234";
 
     InputStream inputStream = new ByteArrayInputStream(fakeInput.getBytes());
     Scanner sc = new Scanner(inputStream);
+
+
+
+    if (comprobarPass(sc, pass))
+        System.out.println("Si");
+    else
+        System.out.println("No");
+}
+
+private static boolean comprobarPass(Scanner sc, String pass){
 
     String texto;
 
@@ -18,8 +29,9 @@ public static void main(String[] args){
         texto = sc.nextLine();
         System.out.println("He recibido " + texto);
 
-    }while (!texto.equals(pass));
-}
+    }while (!texto.equals(pass) && sc.hasNext());
 
+    return texto.equals(pass);
+}
 
 }
